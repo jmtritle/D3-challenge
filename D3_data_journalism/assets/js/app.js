f3f812ff7550533006770c2105e4297a2a1c2c7f
jmtritle <57117317+jmtritle@users.noreply.github.com>
2020-04-05 14:45:59 -0400

    small update to app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,12 @@
 // @TODO: YOUR CODE HERE!
+
+d3.csv("../assets/data/data.csv", function(data) {
+    console.log(data);
+    console.log(data.poverty);
+    console.log(data.smokes);
+    console.log(data.abbr)
+});
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
